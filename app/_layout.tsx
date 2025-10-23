f38c5bfde7a1775cb8d5981f06600a65f72552aa
bhimanshukalra <bhimanshukalra@gmail.com>
2025-10-23 19:59:51 +0530

    Chore: added assets and font setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hide();
+    }
+    if (error) {
+      Alert.alert("An error occurred while loading fonts", error.message);
+    }
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
